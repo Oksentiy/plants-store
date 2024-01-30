76f53c918ddbccd87850185605d16d0de0bdc47c
Oksentiy <94550267+Oksentiy@users.noreply.github.com>
2024-01-30 11:38:36 +0100

    a bit of navbar code

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,11 +1,11 @@
 import { NavBar } from "../reusableComponents/NavBar"
-
+import './mainPage.css'
 
 
 export const MainPage = () => {
   return (
-    <>
+    <div className="container">
       <NavBar/>
-    </>
+    </div>
   )
 }
